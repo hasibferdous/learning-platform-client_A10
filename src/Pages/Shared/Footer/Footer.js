1e36a5c8e72ff1ea986440f0c35167437fa0c457
hasibferdous <hasibferdous.ict@gmail.com>
2022-10-27 04:48:57 +0600

    footer section updated

--- src/Pages/Shared/Footer/Footer.js
+++ src/Pages/Shared/Footer/Footer.js
@@ -1,5 +1,3 @@
-import { GoogleAuthProvider } from 'firebase/auth';
-import React, { useContext } from 'react';
 import Button from 'react-bootstrap/Button';
 import ButtonGroup from 'react-bootstrap/ButtonGroup';
 import { FaGoogle } from 'react-icons/fa';
@@ -9,35 +7,17 @@ import { FaFacebook } from 'react-icons/fa';
 import { FaTwitter } from 'react-icons/fa';
 import { FaYoutube } from 'react-icons/fa';
 import { FaWhatsapp } from 'react-icons/fa';
-import { AuthContext } from '../../../Contexts/AuthProvider/AuthProvider';
+
 
 const Footer = () => {
 
-    const {providerLogin} = useContext(AuthContext);
 
-    const googleProvider = new GoogleAuthProvider();
-
-    const handleGoogleSignIn = () =>{
-        providerLogin(googleProvider)
-        .then(result =>{
-            const user = result.user;
-            console.log(user);
-
-        })
-        .catch(error => console.error(error))
-
-    }
 
 
 
     return (
         <div className='text-center mt-3 bg-dark text-white'>
-            <h5 className='pt-5'>Login With</h5>
-            <ButtonGroup horizontal>
-            <Button onClick={handleGoogleSignIn} className='mb-2 rounded-5' variant="outline-info"><FaGoogle></FaGoogle> Google</Button>
-            <Button className='mb-2 ms-2 rounded-5' variant="outline-info"><FaGithub></FaGithub> Github</Button>
-            <Button className='mb-2 ms-2 rounded-5' variant="outline-info"><FaFacebook></FaFacebook> Facebook</Button>
-            </ButtonGroup>
+
             <h5 className='mt-3'>Find us on</h5>
             <ButtonGroup>
             <Button className='mb-5 ms-2 rounded-5' variant="outline-info"><FaFacebook></FaFacebook></Button>
@@ -46,6 +26,7 @@ const Footer = () => {
             <Button className='mb-5 ms-2 rounded-5' variant="outline-info"><FaWhatsapp></FaWhatsapp></Button>
             <Button className='mb-5 ms-2 rounded-5' variant="outline-info"><FaYoutube></FaYoutube></Button>
             </ButtonGroup>
+            <p>Copywrite hf@2022</p>
         </div>
     );
 };
